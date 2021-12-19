fb2b84fee5c0c9a1806e84f3f282602fb45b9437
naum088 <6110814n@gmail.com>
2021-12-19 09:56:18 +0300

    save new task

---
+++ learnJS/index.js
@@ -0,0 +1,28 @@
+//task5.2
+// let numFirst = +prompt('Enter number', '5');
+// let numSecond = +prompt('Enter number', '3');
+// let sum = numFirst + numSecond;
+// console.log(sum);
+
+// function numEnter() {
+//    for (let i=0; i <= 1; i++) {
+//       let num = +prompt('Enter number', '2');
+//       if (isNaN(num)) {
+//          let num = +prompt('Enter number', '2');
+//          i--
+//       } else if (num == '' || num == null) {
+//          console.log(null);
+//       } else {
+//          console.log(num);
+//       }
+//    }
+// }
+// numEnter();
+
+// function randomInteger(min, max) {
+//    let numRandom = Math.floor(Math.random() * (max - min)) + min;
+//    console.log('Random number = ' + numRandom);
+//    let numRandom1 = Math.floor(Math.random() * 10)
+//    console.log('Random number One = ' + numRandom1);
+// }
+// randomInteger(1, 10);
