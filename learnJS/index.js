df30648508ff3bea305674fd05b0a8e2ca16313f
Andrei Naumenko <6110814n@gmail.com>
2021-12-25 01:16:35 +0300

    save js task1-3

--- learnJS/index.js
+++ learnJS/index.js
@@ -143,3 +143,24 @@
 //    console.log(arr);
 // }
 // shuffle();
+
+//item 5.7
+// function unique(arr) {
+//    return Array.from(new Set(arr));
+// }
+// let values = ["Hare", "Krishna", "Hare", "Krishna",
+//    "Krishna", "Krishna", "Hare", "Hare", ":-O"
+// ];
+// console.log(unique(values));
+
+// function aclean(arr) {
+//    let obj = {};
+//    for (let i = 0; i < arr.length; i++) {
+//       let sorted = arr[i].toLowerCase().split("").sort().join("");
+//       obj[sorted] = arr[i];
+//    }
+//    return Object.values(obj);
+// }
+// let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+// console.log(aclean(arr));
+
